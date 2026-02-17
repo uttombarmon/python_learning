485c91d587e0de9f1f55468d4b08bff436d1b0a7
uttombarmon <uttombarmondev@gmail.com>
2026-02-17 22:25:21 +0600

    excercises basics

---
+++ basics/basics.py
@@ -0,0 +1,34 @@
+import sys
+
+# check version
+print(sys.version)
+
+# Python Indentation
+if 3>2:
+  print("3 is greater than 2!")
+
+# Inline statements by semicolon
+print("First"); print("second"); print("Third")
+
+# newline and with out new line
+print("Hello \nWorld!")
+print("Hello", end=" ")
+print("World!")
+
+# print numbers
+print(12)
+
+#print string
+print("Print strings!")
+
+# print string and number
+print("Hey I'am ", 90, "years old")
+
+# comments
+"""
+this is
+multi line
+comments
+"""
+
+print("This is inline  comments") # this is inline comments
---
+++ basics/variables.py
@@ -0,0 +1,35 @@
+# variables
+x = 1
+print(x)
+y = 3.2
+print(y)
+name = "Gabbar" 
+print(name)
+
+# multiple variables
+x1,y1,z1 = 1, "game", 3
+print(x1, y1, z1)
+
+# 1 value in multiple variable assign
+x2 = y2 = z2 = 3
+print(x2, y2, z2)
+
+# unpack collections
+
+fruits =["apple", "banana", "guava"]
+fruits1, fruits2, fruits3 = fruits
+print(fruits1, fruits2, fruits3)
+
+# add multiple string
+first1 = "Bird "
+first2 = "Can "
+first3 = "Fly! "
+print(first1+first2+first3)
+
+# variable scope 
+e = "true" # global variable
+def myFunc():
+  e = 4 # local variable
+  print("Local e= ", e)
+myFunc()
+print("Global e= ", e)
