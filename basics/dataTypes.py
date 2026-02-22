b2e0456af35875979fb4e16d95777d152d7cb59e
uttombarmon <uttombarmondev@gmail.com>
2026-02-22 18:12:33 +0000

    add data type check example

---
+++ basics/dataTypes.py
@@ -0,0 +1,20 @@
+# Data Type Check
+# Text Type:	str
+
+# Numeric Types:	int, float, complex
+x = 2  # Numeric: Integer
+# print(type(x))
+y = 2.3 # Numeric: Float
+# print(type(y))
+z = 2j # Numeric:Complex
+# print(type(z))
+
+# Sequence Types:	list, tuple, range
+x = ["Game1", "Game1", "Game1"] # 
+print(type(x))
+
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
